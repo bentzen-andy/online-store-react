b75fa9c064452d56d2c4b10b72ecb3b6ff310158
Andrew Bentzen <bentzen.an@gmail.com>
2022-04-02 15:02:02 -0400

    preps for production

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
-import React, { useEffect } from "react";
-import css from "./App.module.css";
+import React, { useEffect } from 'react';
+import css from './App.module.css';
 
 function App() {
   useEffect(() => {
-    console.log("useEffect");
-    fetch("http://localhost:8080/products", {
+    console.log('useEffect - http://localhost:8080/products');
+    fetch('http://localhost:8080/products', {
       headers: {
-        Accept: "application/json",
+        Accept: 'application/json',
       },
     })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   }, []);
 
-  return <div className={css["app"]}>index</div>;
+  useEffect(() => {
+    console.log('useEffect - https://atb-online-store-api/products');
+    fetch('https://atb-online-store-api/products', {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <div className={css['app']}>index</div>;
 }
 
 export default App;
